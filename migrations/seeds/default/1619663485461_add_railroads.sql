52b6de11201644131c02fac91072f5524d11b791
Adron Hall <adronhall@gmail.com>
2021-04-29 10:45:26 -0700

    still not connecting to SQL Server, but current wip.

--- migrations/seeds/default/1619663485461_add_railroads.sql
+++ migrations/seeds/default/1619663485461_add_railroads.sql
@@ -1,3 +1,3 @@
-insert into railroad_systems.railroads (name) values ('Amtrak', true, 'https://www.amtrak.com/');
-insert into railroad_systems.railroads (name) values ('Sound Transit', true, 'https://www.soundtransit.org/');
-insert into railroad_systems.railroads (name) values ('Caltrain', true, 'https://www.caltrain.com/');
+insert into railroad_systems.railroads (name, active, uri) values ('Amtrak', true, 'https://www.amtrak.com/');
+insert into railroad_systems.railroads (name, active, uri) values ('Sound Transit', true, 'https://www.soundtransit.org/');
+insert into railroad_systems.railroads (name, active, uri) values ('Caltrain', true, 'https://www.caltrain.com/');
