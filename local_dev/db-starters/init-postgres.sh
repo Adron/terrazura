ee01dd7e912835f9809262c8413734a31e01f985
Adron Hall <adronhall@gmail.com>
2021-05-25 14:59:49 -0700

    Removed the secondary database.

--- local_dev/db-starters/init-postgres.sh
+++ local_dev/db-starters/init-postgres.sh
@@ -3,7 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE coredb;
-    CREATE DATABASE applications;
     GRANT ALL PRIVILEGES ON DATABASE coredb TO postgres;
-    GRANT ALL PRIVILEGES ON DATABASE applications TO postgres;
 EOSQL
